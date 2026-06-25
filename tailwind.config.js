e206734c569ac6dc75333e49a7b459cdbb377835
SamuelRoa <samuelroa62@gmail.com>
2026-06-25 19:09:19 -0400

    posible arreglo de imagenes en la web

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         andes: {
           forest: '#113220', // Verde bosque profundo
           slate: '#4A5568',  // Gris pizarra
-          bone: '#FAF9F5',   // Blanco hueso
+          bone: '#FAF9F5',   // Blanco hueso xd
           gold: '#C5A059',   // Oro envejecido
           goldHover: '#B89047'
         }
